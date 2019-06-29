dac7085dda24548abbae8e4dbe1862f048449181
tkucharski <tomasz.kucharski@coreservices.pl>
2019-06-29 20:05:58 +0200

    dodanie nowej interwencji - wersja dzialajaca :) f**k yeah

--- src/main/java/com/pathos/dev/animals/handlers/AddInterventionHandler.java
+++ src/main/java/com/pathos/dev/animals/handlers/AddInterventionHandler.java
@@ -5,72 +5,53 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.pathos.dev.animals.domain.InterventionRequest;
+import com.pathos.dev.animals.domain.InterventionsQueryResponse;
 
-import java.io.*;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
-public class AddInterventionHandler implements RequestStreamHandler {
+public class AddInterventionHandler implements RequestHandler<InterventionRequest, InterventionsQueryResponse> {
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
-
-        JsonParser parser = new JsonParser();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        JsonObject responseJson = new JsonObject();
-        JsonObject responseBodyJson = new JsonObject();
-        JsonObject headerJson = new JsonObject();
+    public InterventionsQueryResponse handleRequest(InterventionRequest interventionRequest, Context context) {
 
         try {
-            JsonObject event = (JsonObject) parser.parse(reader);
             AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+            Map<String, AttributeValue> attributeValues = new HashMap<>();
 
-            String id = null;
-            if (event.get("body") != null) {
-                InterventionRequest request = new InterventionRequest(event.get("body"));
-                Map<String, AttributeValue> attributeValues = new HashMap<>();
+            attributeValues.put("id", new AttributeValue().withS(UUID.randomUUID().toString()));
 
-                id = UUID.randomUUID().toString();
+            attributeValues.put("requestDate", new AttributeValue().withN(Long.toString(new Date().getTime())));
+            attributeValues.put("creationDate", new AttributeValue().withN(Long.toString(new Date().getTime() + 100)));
+            attributeValues.put("mofificationDate", new AttributeValue().withN(Long.toString(new Date().getTime() + 100)));
 
-                attributeValues.put("id", new AttributeValue().withS(UUID.randomUUID().toString()));
+            if (interventionRequest.getName() != null)
+                attributeValues.put("name", new AttributeValue().withS(interventionRequest.getName()));
+            if (interventionRequest.getSurname() != null)
+                attributeValues.put("surname", new AttributeValue().withS(interventionRequest.getSurname()));
+            if (interventionRequest.getDescription() != null)
+                attributeValues.put("description", new AttributeValue().withS(interventionRequest.getDescription()));
+            if (interventionRequest.getPhoneNumber() != null)
+                attributeValues.put("phoneNumber", new AttributeValue().withS(interventionRequest.getPhoneNumber()));
+            if (interventionRequest.getParcel() != null)
+                attributeValues.put("parcel", new AttributeValue().withS(interventionRequest.getParcel()));
+            if (interventionRequest.getHouseNumber() != null)
+                attributeValues.put("houseNumber", new AttributeValue().withS(interventionRequest.getHouseNumber()));
+            if (interventionRequest.getCity() != null)
+                attributeValues.put("city", new AttributeValue().withS(interventionRequest.getCity()));
+            if (interventionRequest.getStreet() != null)
+                attributeValues.put("street", new AttributeValue().withS(interventionRequest.getStreet()));
+            attributeValues.put("interventionStatus", new AttributeValue().withS(String.valueOf(1))); //getStatus
 
-                attributeValues.put("requestDate", new AttributeValue().withN(id));
-                attributeValues.put("creationDate", new AttributeValue().withN(Long.toString(new Date().getTime() + 100)));
-                attributeValues.put("mofificationDate", new AttributeValue().withN(Long.toString(new Date().getTime() + 100)));
+            attributeValues.values().removeIf(Objects::isNull);
 
-                attributeValues.put("name", new AttributeValue().withS(request.getName()));
-                attributeValues.put("surname", new AttributeValue().withS(request.getSurname()));
-                attributeValues.put("description", new AttributeValue().withS(request.getDescription()));
-                attributeValues.put("phoneNumber", new AttributeValue().withS(request.getPhoneNumber()));
-                attributeValues.put("parcel", new AttributeValue().withS(request.getParcel()));
-                attributeValues.put("houseNumber", new AttributeValue().withS(request.getHouseNumber()));
-                attributeValues.put("city", new AttributeValue().withS(request.getCity()));
-                attributeValues.put("street", new AttributeValue().withS(request.getStreet()));
-                attributeValues.put("interventionStatus", new AttributeValue().withS(String.valueOf(request.getStatus())));
-
-                PutItemRequest putItemRequest = new PutItemRequest().withTableName("Interventions").withItem(attributeValues);
-                client.putItem(putItemRequest);
-
-            }
-            responseBodyJson.addProperty("message", "new intervention created " + id);
-
-            headerJson.addProperty("x-custom-header", "OK");
-            responseJson.add("headers", headerJson);
-            responseJson.add("body", responseBodyJson);
+            PutItemRequest putItemRequest = new PutItemRequest().withTableName("Interventions").withItem(attributeValues);
+            client.putItem(putItemRequest);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
-        writer.write(responseJson.toString());
-        writer.close();
+        return new InterventionsQueryResponse();
     }
 }
