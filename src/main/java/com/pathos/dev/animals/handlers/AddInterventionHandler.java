d958b5293e14e2c2ee753f94891be21569269e89
tkucharski <tomasz.kucharski@coreservices.pl>
2019-06-29 15:05:40 +0200

    handler bez refactoringu

--- src/main/java/com/pathos/dev/animals/handlers/AddInterventionHandler.java
+++ src/main/java/com/pathos/dev/animals/handlers/AddInterventionHandler.java
@@ -1,12 +1,12 @@
 package com.pathos.dev.animals.handlers;
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.JsonObject;
@@ -32,19 +32,38 @@ public class AddInterventionHandler implements RequestStreamHandler {
 
             AmazonDynamoDBClient client = new AmazonDynamoDBClient(new DefaultAWSCredentialsProviderChain());
             DynamoDB dynamoDB = new DynamoDB(client);
-            Table table = dynamoDB.getTable("intervention");
-            Item item = new Item();
-            
-            if(event.get("body") != null){
+
+            if (event.get("body") != null) {
                 InterventionRequest request = new InterventionRequest(event.get("body"));
+
+                Item item = new Item()
+                        .with("creationDate", new AttributeValue().withS(request.getCreationDate().toString()))
+                        .with("mofificationDate", new AttributeValue().withS(request.getMofificationDate().toString()))
+                        .with("name", new AttributeValue().withS(request.getName()))
+                        .with("surname", new AttributeValue().withS(request.getSurname()))
+                        .with("description", new AttributeValue().withS(request.getDescription()))
+                        .with("phoneNumber", new AttributeValue().withS(request.getPhoneNumber()))
+                        .with("parcel", new AttributeValue().withS(request.getParcel()))
+                        .with("houseNumber", new AttributeValue().withS(request.getHouseNumber()))
+                        .with("city", new AttributeValue().withS(request.getCity()))
+                        .with("street", new AttributeValue().withS(request.getStreet()));
+
+                PutItemSpec itemSpec = new PutItemSpec()
+                        .withItem(item)
+                        .withConditionExpression("attribute_not_exists(#ps)")
+                        .withNameMap(new NameMap()
+                                .with("#ps", "PartitionSpec"));
+
+                dynamoDB.getTable("intervention").putItem(itemSpec);
+
             }
-            responseBodyJson.addProperty("message", "New item created");
+            responseBodyJson.addProperty("message", "new intervention created");
 
             headerJson.addProperty("x-custom-header", "my custom header value");
             responseJson.add("headers", headerJson);
             responseJson.add("body", responseBodyJson);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
