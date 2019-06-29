f36f6fe1267b4855dffcc16aa4ee0a12f1cac831
minidmnv <minidmnv@gmail.com>
2019-06-29 19:42:42 +0200

    Get interventions list of interventions

--- src/main/java/com/pathos/dev/animals/domain/InterventionsQueryResponse.java
+++ src/main/java/com/pathos/dev/animals/domain/InterventionsQueryResponse.java
@@ -1,10 +1,13 @@
 package com.pathos.dev.animals.domain;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
 import java.util.List;
+import java.util.Map;
 
 public class InterventionsQueryResponse {
 
     public Integer count;
-    public List<InterventionRequest> interventions;
+    public List<Map<String, AttributeValue>> interventions;
 
 }
--- src/main/java/com/pathos/dev/animals/handlers/UploadFileHandler.java
+++ src/main/java/com/pathos/dev/animals/handlers/UploadFileHandler.java
@@ -1,9 +1,5 @@
 package com.pathos.dev.animals.handlers;
 
-import com.pathos.dev.animals.service.UploadClient;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,7 +8,6 @@ import java.nio.file.Paths;
 public class UploadFileHandler {
 
     public static void main(String[] args) {
-        UploadClient uploadClient = new UploadClient();
 
         Path path = Paths.get("/home/lbujak/file.txt");
         String name = "file.txt";
@@ -23,9 +18,5 @@ public class UploadFileHandler {
             content = Files.readAllBytes(path);
         } catch (final IOException e) {
         }
-        MultipartFile result = new MockMultipartFile(name,
-                originalFileName, contentType, content);
-
-        uploadClient.uploadFile(result);
     }
 }
