7b908ce1758c5bc2ea0b280c610f9c721bed0945
tkucharski <tomasz.kucharski@coreservices.pl>
2019-06-30 02:23:01 +0200

    poprawka literówki

--- src/main/java/com/pathos/dev/animals/handlers/EditInterventionHandler.java
+++ src/main/java/com/pathos/dev/animals/handlers/EditInterventionHandler.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class EditInterventionHander implements RequestHandler<InterventionRequest, InterventionsQueryResponse> {
+public class EditInterventionHandler implements RequestHandler<InterventionRequest, InterventionsQueryResponse> {
 
     @Override
     public InterventionsQueryResponse handleRequest(InterventionRequest interventionRequest, Context context) {
